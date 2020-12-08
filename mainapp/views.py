f05b404c390b0163e86232dc86c17a5563fe9087
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2020-12-08 23:23:15 +0300

    lesson2

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,3 +7,18 @@ def index(request):
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+def test_context(request):
+    context = {
+        'title': 'добро пожаловать',
+        'username': 'Alex Alex',
+        'products': [
+            {'name': 'Черная худи', 'price': '2 990 руб.'},
+            {'name': 'Джинсы', 'price': '5 900 руб.'},
+        ],
+        'promotion': True,
+        'promotion_products': [
+            {'name': 'Туфли', 'price': '10 000 руб.'},
+        ],
+    }
+    return render(request, 'mainapp/context.html', context)
